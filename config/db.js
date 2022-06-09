b399c34e6d112d5bcc392b00fb569e2663b31a02
Johann LEHUEDE <lehuedejo@gmail.com>
2022-06-09 16:51:58 +0200

    first commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
+console.log(process.env.PASSWORD);
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'mad-kudu',
-  password: '1934Afed',
+  password: process.env.PASSWORD,
 });
 
 module.exports = pool.promise();
